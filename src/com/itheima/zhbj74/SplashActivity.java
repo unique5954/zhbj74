190d733fb51b912582e2132af1a41ac56845d65f
liaodajun <595499714@qq.com>
2017-08-28 17:10:11 +0800

    第一次代码提交

---
+++ src/com/itheima/zhbj74/SplashActivity.java
@@ -0,0 +1,52 @@
+package com.itheima.zhbj74;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.RelativeLayout;
+
+public class SplashActivity extends Activity {
+
+	private RelativeLayout rlRoot;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        
+        rlRoot = (RelativeLayout) findViewById(R.id.rl_root);
+        
+        //旋转动画
+        RotateAnimation animRotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        animRotate.setDuration(1000);
+        animRotate.setFillAfter(true);
+        
+        //缩放动画
+        ScaleAnimation animScale = new ScaleAnimation(0, 1, 0, 1, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        animScale.setDuration(1000);
+        animScale.setFillAfter(true);
+        
+        //渐变动画
+        AlphaAnimation animAlpha = new AlphaAnimation(0, 1);
+        animAlpha.setDuration(2000);
+        animAlpha.setFillAfter(true);
+        
+        //动画集合
+        AnimationSet set = new AnimationSet(true);
+        set.addAnimation(animRotate);
+        set.addAnimation(animScale);
+        set.addAnimation(animAlpha);
+        
+        //启动动画
+        rlRoot.startAnimation(set);
+        
+    }
+
+    
+
+    
+}
